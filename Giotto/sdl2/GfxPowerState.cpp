f2f4dae9355dd650cb4a7cb96d1c02377e0115a0
georgeoros <georgeoros@georges-macbook.local>
2017-04-23 10:53:55 +0300

    Added class GfxPowerState

---
+++ Giotto/sdl2/GfxPowerState.cpp
@@ -0,0 +1,81 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
+
+#include "GfxPowerState.hpp"
+
+GfxPowerState::GfxPowerState() : GfxRootClass("GfxPowerState")
+{
+    value_ = static_cast<SdlType>(GfxPowerStateValues::stateUnknown);
+}
+
+GfxPowerState::GfxPowerState(GfxPowerStateValues value) : GfxRootClass("GfxPowerState")
+{
+    value_ = static_cast<SdlType>(value);
+}
+
+GfxPowerState::GfxPowerState(SdlType value) : GfxRootClass("GfxPowerState")
+{
+    value_ = value;
+}
+
+GfxPowerState::GfxPowerState(GfxPowerState const& other) : GfxRootClass("GfxPowerState")
+{
+    value_  =other.value_;
+}
+
+GfxPowerState::GfxPowerState(GfxPowerState&& other) : GfxRootClass("GfxPowerState")
+{
+    value_ = other.value_;
+    // Delete other's value
+    other.value_ = static_cast<SdlType>(GfxPowerStateValues::stateUnknown);
+}
+
+GfxPowerState& GfxPowerState::operator=(GfxPowerState const& other)
+{
+    if (this != &other)
+    {
+        value_ = other.value_;
+    }
+    return *this;
+}
+
+GfxPowerState& GfxPowerState::operator=(GfxPowerState&& other)
+{
+    if (this != &other)
+    {
+        value_ = other.value_;
+        // Delete other's value
+        other.value_ = static_cast<SdlType>(GfxPowerStateValues::stateUnknown);
+    }
+    return *this;
+}
+
+GfxPowerState::SdlType GfxPowerState::getAsSdlType(void) const
+{
+    return value_;
+}
+
+GfxPowerState::SdlTypePtr GfxPowerState::getAsSdlTypePtr(void) const
+{
+    return (SdlTypePtr)value_;
+}
---
+++ Giotto/sdl2/GfxPowerState.hpp
@@ -0,0 +1,60 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
+
+#ifndef GfxPowerState_hpp
+#define GfxPowerState_hpp
+
+#include "GfxRootClass.hpp"
+#include "GfxSdlHeader.hpp"
+
+class GfxPowerState final : public GfxRootClass
+{
+public:
+    typedef SDL_PowerState SdlType;
+    typedef SDL_PowerState* SdlTypePtr;
+    
+    enum class GfxPowerStateValues : int
+    {
+        stateUnknown = SDL_POWERSTATE_UNKNOWN,
+        stateOnBattery = SDL_POWERSTATE_ON_BATTERY,
+        stateNoBattery = SDL_POWERSTATE_NO_BATTERY,
+        stateCharging = SDL_POWERSTATE_CHARGING,
+        stateCharged = SDL_POWERSTATE_CHARGED
+    };
+    
+    GfxPowerState();
+    GfxPowerState(GfxPowerStateValues value);
+    GfxPowerState(SdlType value);
+    GfxPowerState(GfxPowerState const& other);
+    GfxPowerState(GfxPowerState&& other);
+        
+    GfxPowerState& operator=(GfxPowerState const& other);
+    GfxPowerState& operator=(GfxPowerState&& other);
+    
+    SdlType getAsSdlType(void) const;
+    SdlTypePtr getAsSdlTypePtr(void) const;
+private:
+    SdlType value_;
+};
+
+#endif /* GfxPowerState_hpp */
