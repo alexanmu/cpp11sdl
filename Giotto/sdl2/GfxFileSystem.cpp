1e75d4fbaf19ddf2eca00c18fb2ad4af6b62a900
georgeoros <georgeoros@users.noreply.github.com>
2017-04-25 09:31:13 +0300

    Add class GfxFileSystem

---
+++ Giotto/sdl2/GfxFileSystem.cpp
@@ -0,0 +1,73 @@
+/*
+  Giotto
+  Copyright (C) 2017 George Oros
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  See copyright notice at http://lidsdl.org/license.php
+*/
+
+#include "GfxFileSystem.hpp"
+#include "GfxSdlHeader.hpp"
+
+GfxFileSystem::GfxFileSystem() : GfxRootClass("GfxFileSystem")
+{
+    // Nothing to do
+}
+
+std::string GfxFileSystem::getBasePath(void) const
+{
+    char * base_path = NULL;
+    std::string str;
+
+    str = SDL_GetBasePath();
+    if (base_path != NULL)
+    {
+        str = base_path;
+    }
+    else
+    {
+        str = getThisFolder();
+    }
+    return str;
+}
+
+std::string GfxFileSystem::getPrefPath(const std::string& organization,const std::string& appname) const
+{
+    char * base_path = NULL;
+    std::string str;
+
+    base_path = SDL_GetPrefPath(organization.c_str(),appname.c_str());
+    if (base_path != NULL)
+    {
+        str = base_path;
+    }
+    else
+    {
+        str = getThisFolder();
+    }
+    return str;
+}
+
+std::string GfxFileSystem::getThisFolder(void) const
+{
+#ifdef __windows_machine
+    return ".\\";
+#else
+    return "./";
+#endif
+}
