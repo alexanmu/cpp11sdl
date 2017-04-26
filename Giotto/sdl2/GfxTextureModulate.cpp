a4e58b6c38c05a75b4bfd91326f8a7ff19d97252
georgeoros <georgeoros@georges-macbook.local>
2017-04-26 17:43:21 +0300

    Add class GfxTextureModulate

---
+++ Giotto/sdl2/GfxTextureModulate.cpp
@@ -0,0 +1,127 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
+
+#include "GfxTextureModulate.hpp"
+
+GfxTextureModulate::GfxTextureModulate() : GfxRootClass("GfxTextureModulate")
+{
+    modulate_ = static_cast<SdlType>(GfxTextureModulateValues::modulateNone);
+}
+
+GfxTextureModulate::GfxTextureModulate(GfxTextureModulateValues flags) : GfxRootClass("GfxTextureModulate")
+{
+    modulate_ = static_cast<SdlType>(flags);
+}
+
+GfxTextureModulate::GfxTextureModulate(SdlType flags) : GfxRootClass("GfxTextureModulate")
+{
+    modulate_ = flags;
+}
+
+GfxTextureModulate::GfxTextureModulate(SdlTypePtr flags) : GfxRootClass("GfxTextureModulate")
+{
+    modulate_ = *flags;
+}
+
+GfxTextureModulate::GfxTextureModulate(GfxTextureModulate const& other) : GfxRootClass("GfxTextureModulate")
+{
+    modulate_ = other.modulate_;
+}
+
+GfxTextureModulate::GfxTextureModulate(GfxTextureModulate&& other) : GfxRootClass("GfxTextureModulate")
+{
+    modulate_ = other.modulate_;
+    // Delete other's data
+    other.modulate_ = static_cast<SdlType>(GfxTextureModulateValues::modulateNone);
+}
+
+GfxTextureModulate& GfxTextureModulate::operator=(GfxTextureModulate const& other)
+{
+    if (this != &other)
+    {
+        modulate_ = other.modulate_;
+    }
+    return *this;
+}
+
+GfxTextureModulate& GfxTextureModulate::operator=(GfxTextureModulate&& other)
+{
+    if (this != &other)
+    {
+        modulate_ = other.modulate_;
+        // Delete other's data
+        other.modulate_ = static_cast<SdlType>(GfxTextureModulateValues::modulateNone);
+    }
+    return *this;
+}
+
+bool GfxTextureModulate::isNone(void) const
+{
+    return (modulate_ == static_cast<SdlType>(GfxTextureModulateValues::modulateNone));
+}
+
+bool GfxTextureModulate::isColor(void) const
+{
+    uint32_t r = modulate_ & SDL_TEXTUREMODULATE_COLOR;
+
+    return (r != 0);
+}
+
+bool GfxTextureModulate::isAlpha(void) const
+{
+    uint32_t r = modulate_ & SDL_TEXTUREMODULATE_ALPHA;
+
+    return (r != 0);
+}
+
+void GfxTextureModulate::setColor(void)
+{
+    modulate_ = static_cast<SdlType>(modulate_ | SDL_TEXTUREMODULATE_COLOR);
+}
+
+void GfxTextureModulate::resetColor(void)
+{
+    modulate_ = static_cast<SdlType>(modulate_ & ~SDL_TEXTUREMODULATE_COLOR);
+}
+
+void GfxTextureModulate::setAlpha(void)
+{
+    modulate_ = static_cast<SdlType>(modulate_ | SDL_TEXTUREMODULATE_ALPHA);
+}
+
+void GfxTextureModulate::resetAlpha(void)
+{
+    modulate_ = static_cast<SdlType>(modulate_ & ~SDL_TEXTUREMODULATE_ALPHA);
+}
+
+GfxTextureModulate::SdlType GfxTextureModulate::getAsSdlType(void) const
+{
+    return modulate_;
+}
+
+GfxTextureModulate::SdlTypePtr GfxTextureModulate::getAsSdlTypePtr(void) const
+{
+    return (SdlTypePtr)&modulate_;
+}
+
+/* EOF */
---
+++ Giotto/sdl2/GfxTextureModulate.hpp
@@ -0,0 +1,69 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
+
+#ifndef GfxTextureModulate_hpp
+#define GfxTextureModulate_hpp
+
+#include "GfxRootClass.hpp"
+#include "GfxSdlHeader.hpp"
+
+class GfxTextureModulate final : public GfxRootClass
+{
+public:
+    typedef SDL_RendererFlags SdlType;
+    typedef SDL_RendererFlags* SdlTypePtr;
+
+    enum class GfxTextureModulateValues : int
+    {
+        modulateNone = SDL_TEXTUREMODULATE_NONE,
+        modulateColor = SDL_TEXTUREMODULATE_COLOR,
+        modulateAlpha = SDL_TEXTUREMODULATE_ALPHA
+    };
+
+    GfxTextureModulate();
+    explicit GfxTextureModulate(GfxTextureModulateValues modulate);
+    explicit GfxTextureModulate(SdlType modulate);
+    explicit GfxTextureModulate(SdlTypePtr modulate);
+    GfxTextureModulate(GfxTextureModulate const& other);
+    GfxTextureModulate(GfxTextureModulate&& other);
+
+    GfxTextureModulate& operator=(GfxTextureModulate const& other);
+    GfxTextureModulate& operator=(GfxTextureModulate&& other);
+
+    bool isNone(void) const;
+    bool isColor(void) const;
+    bool isAlpha(void) const;
+
+    void setColor(void);
+    void resetColor(void);
+        
+    void setAlpha(void);
+    void resetAlpha(void);
+
+    SdlType getAsSdlType(void) const;
+    SdlTypePtr getAsSdlTypePtr(void) const;
+private:
+    SdlType modulate_;
+};
+
+#endif /* GfxTextureModulate_hpp */
