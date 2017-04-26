6624fbd74f1819194b2a1ef6efe3eb74497a4159
georgeoros <georgeoros@georges-macbook.local>
2017-04-26 20:13:53 +0300

    Add class GfxBool

---
+++ Giotto/sdl2/GfxBool.cpp
@@ -0,0 +1,93 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
+
+#include "GfxBool.hpp"
+
+GfxBool::GfxBool() : GfxRootClass("GfxBool")
+{
+    value_ = static_cast<SdlType>(false);
+}
+
+GfxBool::GfxBool(bool value) : GfxRootClass("GfxBool")
+{
+    value_ = static_cast<SdlType>(value);
+}
+
+GfxBool::GfxBool(SdlType value) : GfxRootClass("GfxBool")
+{
+    value_ = value;
+}
+
+GfxBool::GfxBool(SdlTypePtr value) : GfxRootClass("GfxBool")
+{
+    value_ = *value;
+}
+
+GfxBool::GfxBool(const GfxBool& other) : GfxRootClass("GfxBool")
+{
+    value_ = other.value_;
+}
+
+GfxBool::GfxBool(GfxBool&& other) : GfxRootClass("GfxBool")
+{
+    value_ = other.value_;
+    // Delete other's data
+    other.value_ = static_cast<SdlType>(false);
+}
+
+GfxBool& GfxBool::operator=(const GfxBool& other)
+{
+    if (this != &other)
+    {
+        value_ = other.value_;
+    }
+    return *this;
+}
+
+GfxBool& GfxBool::operator=(GfxBool&& other)
+{
+    if (this != &other)
+    {
+        value_ = other.value_;
+        // Delete other's data
+        other.value_ = static_cast<SdlType>(false);
+    }
+    return *this;
+}
+
+bool GfxBool::getBool(void) const
+{
+    return static_cast<bool>(value_);
+}
+
+GfxBool::SdlType GfxBool::getAsSdlType(void) const
+{
+    return value_;
+}
+
+GfxBool::SdlTypePtr GfxBool::getAsSdlTypePtr(void) const
+{
+    return (SdlTypePtr)&value_;
+}
+
+/* EOF */
---
+++ Giotto/sdl2/GfxBool.hpp
@@ -0,0 +1,54 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
+
+#ifndef GfxBool_hpp
+#define GfxBool_hpp
+
+#include "GfxRootClass.hpp"
+#include "GfxSdlHeader.hpp"
+
+class GfxBool final : public GfxRootClass
+{
+public:
+    typedef SDL_bool SdlType;
+    typedef SDL_bool* SdlTypePtr;
+
+    GfxBool();
+    explicit GfxBool(bool value);
+    explicit GfxBool(SdlType value);
+    explicit GfxBool(SdlTypePtr value);
+    GfxBool(const GfxBool& other);
+    GfxBool(GfxBool&& other);
+
+    GfxBool& operator=(const GfxBool& other);
+    GfxBool& operator=(GfxBool&& other);
+    
+    bool getBool(void) const;
+
+    SdlType getAsSdlType(void) const;
+    SdlTypePtr getAsSdlTypePtr(void) const;
+private:
+    SdlType value_;
+};
+    
+#endif /* GfxBool_hpp */
