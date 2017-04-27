1d46442afa81c9917a8014ca5c576748e97e6614
georgeoros <georgeoros@users.noreply.github.com>
2017-04-27 14:51:03 +0300

    Add class GfxRendererInfo

---
+++ Giotto/sdl2/GfxRendererInfo.cpp
@@ -0,0 +1,158 @@
+/*
+  Giotto
+  Copyright (C) 2017 George Oros
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  See copyright notice at http://lidsdl.org/license.php
+*/
+
+#include <cstring>
+
+#include "GfxRendererInfo.hpp"
+
+GfxRendererInfo::GfxRendererInfo() : GfxRootClass("GfxRendererInfo")
+{
+    info_.name = nullptr;
+    info_.flags = 0;
+    info_.num_texture_formats = 0;
+    for (int i = 0; i < textureFormatsArrayLength; i++)
+    {
+        info_.texture_formats[i] = 0;
+    }
+    info_.max_texture_width = -1;
+    info_.max_texture_height = -1;
+}
+
+GfxRendererInfo::GfxRendererInfo(SdlType info) : GfxRootClass("GfxRendererInfo")
+{
+    info_ = info;
+}
+
+GfxRendererInfo::GfxRendererInfo(const GfxRendererInfo& other) : GfxRootClass("GfxRendererInfo")
+{
+    info_ = other.info_;
+}
+
+GfxRendererInfo::GfxRendererInfo(GfxRendererInfo&& other) : GfxRootClass("GfxRendererInfo")
+{
+    info_ = other.info_;
+    // Delete other's data
+    other.info_.name = nullptr;
+    other.info_.flags = 0;
+    other.info_.num_texture_formats = 0;
+    for (int i = 0; i < textureFormatsArrayLength; i++)
+    {
+        other.info_.texture_formats[i] = 0;
+    }
+    other.info_.max_texture_width = -1;
+    other.info_.max_texture_height = -1;
+}
+
+GfxRendererInfo& GfxRendererInfo::operator=(const GfxRendererInfo& other)
+{
+    if (this != &other)
+    {
+        info_ = other.info_;
+    }
+    return *this;
+}
+
+GfxRendererInfo& GfxRendererInfo::operator=(GfxRendererInfo&& other)
+{
+    if (this != &other)
+    {
+        info_ = other.info_;
+        // Delete other's data
+        other.info_.name = nullptr;
+        other.info_.flags = 0;
+        other.info_.num_texture_formats = 0;
+        for (int i = 0; i < textureFormatsArrayLength; i++)
+        {
+            other.info_.texture_formats[i] = 0;
+        }
+        other.info_.max_texture_width = -1;
+        other.info_.max_texture_height = -1;
+    }
+    return *this;
+}
+
+std::string GfxRendererInfo::getName(void) const
+{
+    std::string str;
+
+    str = info_.name;
+    return str;
+}
+
+uint32_t GfxRendererInfo::getFlags(void) const
+{
+    return info_.flags;
+}
+
+uint32_t GfxRendererInfo::getNumTextureFormats(void) const
+{
+    return info_.num_texture_formats;
+}
+
+GfxRendererInfo::GfxTextureFormats GfxRendererInfo::getTextureFormats(void) const
+{
+    GfxTextureFormats tf;
+    uint32_t max;
+
+    std::memset(reinterpret_cast<void *>(&tf.formats[0]), 0, sizeof(tf.formats));
+    if (info_.num_texture_formats > textureFormatsArrayLength)
+    {
+        max = textureFormatsArrayLength;
+    }
+    else
+    {
+        max = info_.num_texture_formats;
+    }
+    for (uint32_t i = 0; i < max; i++)
+    {
+        tf[i] = info_.texture_formats[i];
+    }
+    return tf;
+}
+
+int32_t GfxRendererInfo::getMaxTextureWidth(void) const
+{
+    return info_.max_texture_width;
+}
+
+int32_t GfxRendererInfo::getMaxTextureHeight(void) const
+{
+    return info_.max_texture_height;
+}
+
+void GfxRendererInfo::set(SdlType info)
+{
+    info_ = info;
+}
+
+GfxRendererInfo::SdlType GfxRendererInfo::getAsSdlType(void) const
+{
+    return (SdlType)info_;
+}
+
+GfxRendererInfo::SdlTypePtr GfxRendererInfo::getAsSdlTypePtr(void) const
+{
+    return (SdlTypePtr)&info_;
+}
+
+/* EOF */
---
+++ Giotto/sdl2/GfxRendererInfo.hpp
@@ -0,0 +1,73 @@
+/*
+  Giotto
+  Copyright (C) 2017 George Oros
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  See copyright notice at http://lidsdl.org/license.php
+*/
+
+#ifndef GfxRendererInfo_hpp
+#define GfxRendererInfo_hpp
+
+#include <cstdint>
+#include <string>
+
+#include "GfxSdlHeader.hpp"
+#include "GfxRootClass.hpp"
+
+class GfxRendererInfo final : public GfxRootClass
+{
+public:
+    typedef SDL_RendererInfo SdlType;
+    typedef SDL_RendererInfo* SdlTypePtr;
+
+    static constexpr int textureFormatsArrayLength = 16;
+
+    struct GfxTextureFormats
+    {
+        uint32_t formats[textureFormatsArrayLength];
+        uint32_t& operator[](int index)
+        {
+            return formats[index];
+        }
+    };
+
+    GfxRendererInfo();
+    explicit GfxRendererInfo(SdlType info);
+    GfxRendererInfo(const GfxRendererInfo& other);
+    GfxRendererInfo(GfxRendererInfo&& other);
+
+    GfxRendererInfo& operator=(const GfxRendererInfo& other);
+    GfxRendererInfo& operator=(GfxRendererInfo&& other);
+
+    std::string getName(void) const;
+    uint32_t getFlags(void) const;
+    uint32_t getNumTextureFormats(void) const;
+    GfxTextureFormats getTextureFormats(void) const;
+    int32_t getMaxTextureWidth(void) const;
+    int32_t getMaxTextureHeight(void) const;
+
+    void set(SdlType info);
+
+    SdlType getAsSdlType(void) const;
+    SdlTypePtr getAsSdlTypePtr(void) const;
+private:
+    SdlType info_;
+};
+
+#endif /* GfxRendererInfo_hpp */
