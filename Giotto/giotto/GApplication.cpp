e25d60d09a310f96585aab576f3124c1fa44e38e
georgeoros <georgeoros@georges-macbook.local>
2017-04-29 21:56:50 +0300

    Update class GApplication

--- Giotto/giotto/GApplication.cpp
+++ Giotto/giotto/GApplication.cpp
@@ -1,57 +1,28 @@
-//
-//  GApplication.cpp
-//  Giotto
-//
-//  Created by George Oros on 4/29/17.
-//  Copyright © 2017 George Oros. All rights reserved.
-//
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
 
 #include "GApplication.hpp"
 
-#include <iostream>
+// Nothing yet. GApplication is pure...
 
-#include "GfxInitQuit.hpp"
-#include "GfxWindow.hpp"
-#include "GfxRenderer.hpp"
-#include "GAbortRetryIgnoreMsgBox.hpp"
-#include "GRetryCancelMsgBox.hpp"
-
-GApplication::GApplication(std::string appName) : GObject()
-{
-    appName_ = appName;
-}
-
-void GApplication::loadAppConfiguration(void)
-{
-    // NOthing to do yet
-}
-
-void GApplication::run(void)
-{
-    GfxInitQuit iq(GfxInitQuit::GfxInitComponent::initEverything);
-    if (iq.getErrorCode() != 0)
-    {
-        std::cout << "Init failed\n";
-        return;
-    }
-    GfxWindowFlags winFlags(GfxWindowFlags::GfxWindowFlagsValues::windowFlagResizable);
-    GfxWindow win(appName_,
-                  GfxWindowPosition(GfxWindowPosition::GfxWindowPositionValues::positionCentered),
-                  GfxWindowPosition(GfxWindowPosition::GfxWindowPositionValues::positionCentered),
-                  WIN_W,
-                  WIN_H,
-                  winFlags);
-    GfxRenderer rend(win);
-
-    GRetryCancelMsgBox g(this,"Error","An error occured. What should I do?");
-    g.showModal();
-    
-    GDialogsConstants sel;
-    
-    sel = g.getSelection();
-    while (sel != GDialogsConstants::kButtonCancel)
-    {
-        g.showModal();
-        sel = g.getSelection();
-    }
-}
+/* EOF */
