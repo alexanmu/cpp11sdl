5c6cf738fffa8148e3a52eb3b4e86535619d8c17
Familia Oros <familiaoros@familias-imac.local>
2017-04-30 17:19:17 +0300

    Add class GOKMsgBox

--- Giotto/GDemoApp.cpp
+++ Giotto/GDemoApp.cpp
@@ -32,6 +32,8 @@
 #include "GRetryCancelMsgBox.hpp"
 #include "GfxError.hpp"
 #include "GForm.hpp"
+#include "GOKMsgBox.hpp"
+#include "GOKCancelMsgBox.hpp"
 
 GDemoApp::GDemoApp() : GApplication()
 {
@@ -61,22 +63,25 @@ void GDemoApp::run(void)
         std::cout << "Init failed\n";
         return;
     }
-    
+
     mainForm_ = std::make_shared<GForm>(appName_);
     mainForm_->createForm();
-    
+
     auto canvas = mainForm_->getFormCanvas();
     canvas->Bar(GfxRect(0, 0, 100, 100), GfxColor(200, 200, 200));
-    
+    canvas->Line(GfxPoint(10,10), GfxPoint(10,90), GfxBgiConstants::vgaWhite());
+    canvas->Line(GfxPoint(10,10), GfxPoint(90,10), GfxBgiConstants::vgaWhite());
+    canvas->Line(GfxPoint(90,10), GfxPoint(90,90), GfxBgiConstants::vgaDarkGray());
+    canvas->Line(GfxPoint(10,90), GfxPoint(90,90), GfxBgiConstants::vgaDarkGray());
     mainForm_->drawForm();
     
-    GRetryCancelMsgBox g(this,"Error","An error occured. What should I do?");
+    GOKCancelMsgBox g(this,"Error","An error occured. What should I do?");
     g.showModal();
     
     GDialogsConstants sel;
     
     sel = g.getSelection();
-    while (sel != GDialogsConstants::kButtonCancel)
+    while (sel != GDialogsConstants::kButtonOk)
     {
         g.showModal();
         sel = g.getSelection();
---
+++ Giotto/gdialogs/GOKMsgBox.cpp
@@ -0,0 +1,91 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+ */
+
+#include <string>
+
+#include "GOKMsgBox.hpp"
+
+#include "GfxMessageBoxFlags.hpp"
+#include "GfxMessageBoxButtonData.hpp"
+#include "GfxMessageBoxButtonFlags.hpp"
+#include "GfxMessageBoxColor.hpp"
+#include "GfxMessageBoxColorType.hpp"
+#include "GfxMessageBoxColorScheme.hpp"
+#include "GfxMessageBoxData.hpp"
+#include "GfxMessageBox.hpp"
+
+GOKMsgBox::GOKMsgBox(GObject* parent) : GObject(), parent_(parent)
+{
+    title_ = "";
+    message_ = "";
+    selection_ = GDialogsConstants::kNoSelection;
+}
+
+GOKMsgBox::GOKMsgBox(GObject* parent,const std::string& title, const std::string& message) : GObject(), parent_(parent)
+{
+    title_ = title;
+    message_ = message;
+    selection_ = GDialogsConstants::kNoSelection;
+}
+
+void GOKMsgBox::showModal(void)
+{
+    GfxMessageBoxFlags flags { GfxMessageBoxFlags::GfxMessageBoxFlagsValues::flagError };
+    
+    GfxMessageBoxButtonData buttons[1] {
+        GfxMessageBoxButtonData(GfxMessageBoxButtonFlags(
+                                                         GfxMessageBoxButtonFlags::GfxMessageBoxButtonFlagsValues::noneDefault), 1, "OK"),
+    };
+    
+    GfxMessageBoxColor colors[static_cast<uint32_t>(GfxMessageBoxColorType::GfxMessageBoxColorTypeValues::colorMax)] = {
+        GfxMessageBoxColor(255, 0, 0),
+        GfxMessageBoxColor(250, 220, 190),
+        GfxMessageBoxColor(192, 92, 9),
+        GfxMessageBoxColor(50, 100, 200),
+        GfxMessageBoxColor(255, 255, 255)
+    };
+    
+    GfxMessageBoxColorScheme colorScheme(colors);
+    
+    GfxMessageBoxData msgBoxData(flags, nullptr, title_, message_, 1, buttons, colorScheme);
+    
+    GfxMessageBox messageBox(msgBoxData);
+    
+    int result = messageBox.showModal();
+    switch (result) {
+        case 1:
+            selection_ = GDialogsConstants::kButtonOk;
+            break;
+        case 2:
+            selection_ = GDialogsConstants::kButtonCancel;
+        default:
+            break;
+    }
+}
+
+GDialogsConstants GOKMsgBox::getSelection(void) const
+{
+    return selection_;
+}
+
+/* EOF */
