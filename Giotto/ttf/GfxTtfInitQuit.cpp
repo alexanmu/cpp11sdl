8720cd641b1b081d2671942bbf52bc1b40539e1c
georgeoros <georgeoros@users.noreply.github.com>
2017-05-17 16:28:08 +0300

    Add class GfxTtfInitQuit

---
+++ Giotto/ttf/GfxTtfInitQuit.cpp
@@ -0,0 +1,58 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+*/
+
+#include "GfxTtfInitQuit.hpp"
+
+namespace gfx
+{
+
+namespace ttf
+{
+
+const char GfxTtfInitQuit::ClassName[] = "GfxTtfInitQuit";
+
+GfxTtfInitQuit::GfxTtfInitQuit() : GfxRootClass(ClassName)
+{
+    errorCode_ = sdl2::TTF_Init();
+}
+
+GfxTtfInitQuit::~GfxTtfInitQuit(void)
+{
+    sdl2::TTF_Quit();
+}
+
+int32_t GfxTtfInitQuit::wasInit(void) const
+{
+    return sdl2::TTF_WasInit();
+}
+
+int32_t GfxTtfInitQuit::getErrorCode(void) const
+{
+    return errorCode_;
+}
+
+}  // namespace ttf
+
+}  // namespace gfx
+
+/* EOF */
