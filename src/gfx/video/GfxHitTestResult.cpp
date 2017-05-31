b3bbe2b747fd34d1bcb142fa0b480e0addb20a75
georgeoros <georgeoros@Georges-MacBook.local>
2017-05-31 19:24:01 +0300

    Add class GfxHitTestResult

---
+++ src/gfx/video/GfxHitTestResult.cpp
@@ -0,0 +1,109 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+*/
+
+#include <string>
+#include <cstdlib>
+
+#include "GfxHitTestResult.hpp"
+#include "GfxSdlHeader.hpp"
+
+namespace gfx
+{
+
+namespace video
+{
+
+const char GfxHitTestResult::ClassName[] = "GfxHitTestResult";
+
+GfxHitTestResult::GfxHitTestResult() : GfxRootClass(ClassName)
+{
+    clear();
+}
+
+GfxHitTestResult::GfxHitTestResult(const ValueType value)
+{
+    value_ = static_cast<SdlType>(value);
+}
+
+GfxHitTestResult::GfxHitTestResult(const SdlType value)
+{
+    value_ = value;
+}
+
+GfxHitTestResult::GfxHitTestResult(GfxHitTestResult const& other) : GfxRootClass(ClassName)
+{
+    value_ = other.value_;
+}
+
+GfxHitTestResult::GfxHitTestResult(GfxHitTestResult&& other) : GfxRootClass(ClassName)
+{
+    value_ = other.value_;
+    // Delete other's data
+    other.clear();
+}
+
+GfxHitTestResult& GfxHitTestResult::operator=(GfxHitTestResult const& other)
+{
+    if (this != &other)
+    {
+        value_ = other.value_;
+    }
+    return *this;
+}
+
+GfxHitTestResult& GfxHitTestResult::operator=(GfxHitTestResult&& other)
+{
+    if (this != &other)
+    {
+        value_ = other.value_;
+        // Delete other's data
+        other.clear();
+    }
+    return *this;
+}
+
+GfxHitTestResult::operator bool() const
+{
+    return true;
+}
+
+GfxHitTestResult::ValueType GfxHitTestResult::getValue(void) const noexcept
+{
+    return static_cast<ValueType>(value_);
+}
+
+void GfxHitTestResult::clear(void) noexcept
+{
+    value_ = static_cast<SdlType>(ValueType::hittestNormal);
+}
+
+GfxHitTestResult::SdlType GfxHitTestResult::getAsSdlType(void) const
+{
+    return value_;
+}
+
+}  // namespace video
+
+}  // namespace gfx
+
+/* EOF */
---
+++ src/gfx/video/GfxHitTestResult.hpp
@@ -0,0 +1,85 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+*/
+
+#ifndef GfxHitTestResult_hpp
+#define GfxHitTestResult_hpp
+
+#include <string>
+#include <cstdint>
+
+#include "GfxRootClass.hpp"
+#include "GfxSdlHeader.hpp"
+
+namespace gfx
+{
+
+namespace video
+{
+
+class GfxHitTestResult final : public GfxRootClass
+{
+public:
+    typedef sdl2::SDL_HitTestResult SdlType;
+
+    static const char ClassName[];
+
+    enum class ValueType : int32_t
+    {
+        hittestNormal = sdl2::SDL_HITTEST_NORMAL,
+        hittestDraggable = sdl2::SDL_HITTEST_DRAGGABLE,
+        hittestResizeTopLeft = sdl2::SDL_HITTEST_RESIZE_TOPLEFT,
+        hittestResizeTop = sdl2::SDL_HITTEST_RESIZE_TOP,
+        hittestResizeTopRight = sdl2::SDL_HITTEST_RESIZE_TOPRIGHT,
+        hittestResizeRight = sdl2::SDL_HITTEST_RESIZE_RIGHT,
+        hittestResizeBottomRight = sdl2::SDL_HITTEST_RESIZE_BOTTOMRIGHT,
+        hittestResizeBottom = sdl2::SDL_HITTEST_RESIZE_BOTTOM,
+        hittestResizeBottomLeft = sdl2::SDL_HITTEST_RESIZE_BOTTOMLEFT,
+        hittestResizeLeft = sdl2::SDL_HITTEST_RESIZE_LEFT
+    };
+
+    GfxHitTestResult();
+    explicit GfxHitTestResult(const ValueType value);
+    explicit GfxHitTestResult(const SdlType value);
+
+    GfxHitTestResult(GfxHitTestResult const& other);
+    GfxHitTestResult(GfxHitTestResult&& other);
+
+    GfxHitTestResult& operator=(GfxHitTestResult const& other);
+    GfxHitTestResult& operator=(GfxHitTestResult&& other);
+
+    virtual explicit operator bool() const;
+
+    ValueType getValue(void) const noexcept;
+
+    void clear(void) noexcept;
+
+    SdlType getAsSdlType(void) const;
+private:
+    SdlType value_;
+};
+
+}  // namespace video
+
+}  // namespace gfx
+
+#endif /* GfxHitTestResult_hpp */
