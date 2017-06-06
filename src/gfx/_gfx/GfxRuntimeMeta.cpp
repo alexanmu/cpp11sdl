cf1b589c5a0aaba04fa01a164550b254b8797358
Familia Oros <familiaoros@Familias-iMac.local>
2017-06-06 20:01:49 +0300

    Add class GfxRuntimeMeta

---
+++ src/gfx/_gfx/GfxRuntimeMeta.cpp
@@ -0,0 +1,148 @@
+/*
+  Giotto
+  Copyright (C) 2017 George Oros
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  See copyright notice at http://lidsdl.org/license.php
+*/
+
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+#include "prettyprint.hpp"
+
+#include "GfxRuntimeMeta.hpp"
+
+namespace gfx
+{
+
+namespace _gfx
+{
+
+inline std::ostream& operator<<(std::ostream& outstream, GfxRuntimeMeta::ClassInfo const& cinfo)
+{
+    assert(cinfo.objectInstanceCount_ >= 0);
+
+    outstream << "(m:" << cinfo.maxObjectInstanceCount_;
+    if (cinfo.objectInstanceCount_ > 0)
+    {
+        outstream << ",i:" << cinfo.objectInstanceCount_;
+    }
+    outstream << ",f:" << cinfo.firstInstanceId_ << ",l:" << cinfo.lastInstanceId_ << ")";
+    return outstream;
+}
+
+GfxRuntimeMeta& GfxRuntimeMeta::getInstance(void) noexcept
+{
+    static GfxRuntimeMeta instance_;
+
+    return instance_;
+}
+
+GfxRuntimeMeta::ClassInfo const& GfxRuntimeMeta::getClassInfo(const int32_t index) noexcept
+{
+    int32_t ctr = 0;
+
+    clear();
+    for (auto it : classUMap)
+    {
+        if (ctr == index)
+        {
+            classInfo_ = it.second;
+            break;
+        }
+        ctr += 1;
+    }
+    return classInfo_;
+}
+
+int32_t GfxRuntimeMeta::getClassCount(void) const noexcept
+{
+    return static_cast<int32_t>(classUMap.size());
+}
+
+void GfxRuntimeMeta::constructObject(const char * className, int32_t instanceId) noexcept
+{
+    assert(className != nullptr);
+    assert(instanceId > 0);
+
+    ClassInfo cinfo;
+
+    auto find_it = classUMap.find(className);
+    if (find_it == classUMap.end())
+    {
+        cinfo.objectInstanceCount_ = 1;
+        cinfo.maxObjectInstanceCount_ = 1;
+        cinfo.firstInstanceId_ = instanceId;
+        cinfo.lastInstanceId_ = -1;
+        classUMap[className] = cinfo;
+    }
+    else
+    {
+        find_it->second.objectInstanceCount_ += 1;
+        find_it->second.maxObjectInstanceCount_ += 1;
+        find_it->second.lastInstanceId_ = instanceId;
+    }
+}
+
+void GfxRuntimeMeta::destructObject(const char * className, int32_t instanceId) noexcept
+{
+    assert(className != nullptr);
+    assert((instanceId > 0) || ((instanceId == -1) && (std::strcmp(className, "$null$") == 0)));
+
+    auto find_it = classUMap.find(className);
+    if (find_it != classUMap.end())
+    {
+        find_it->second.objectInstanceCount_ -= 1;
+        find_it->second.lastInstanceId_ = instanceId;
+    }
+}
+
+std::ostream& GfxRuntimeMeta::printToStream(std::ostream& outstream) const noexcept
+{
+    outstream << classUMap;
+    return outstream;
+}
+
+// Private methods
+GfxRuntimeMeta::GfxRuntimeMeta() noexcept
+{
+    clear();
+}
+
+GfxRuntimeMeta::~GfxRuntimeMeta() noexcept
+{
+    // Nothing to do
+}
+
+void GfxRuntimeMeta::clear(void) noexcept
+{
+    classInfo_.objectInstanceCount_ = -1;
+    classInfo_.maxObjectInstanceCount_ = -1;
+    classInfo_.firstInstanceId_ = -1;
+    classInfo_.lastInstanceId_ = -1;
+}
+
+}  // namespace _gfx
+
+}  // namespace gfx
+
+/* EOF */
---
+++ src/gfx/_gfx/GfxRuntimeMeta.hpp
@@ -0,0 +1,81 @@
+/*
+  Giotto
+  Copyright (C) 2017 George Oros
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  See copyright notice at http://lidsdl.org/license.php
+*/
+
+#ifndef GfxRuntimeMeta_hpp
+#define GfxRuntimeMeta_hpp
+
+#include <stdexcept>
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+#include <ostream>
+
+
+namespace gfx
+{
+
+namespace _gfx
+{
+
+class GfxRuntimeMeta final
+{
+public:
+    struct ClassInfo
+    {
+        int32_t maxObjectInstanceCount_;
+        int32_t objectInstanceCount_;
+        int32_t firstInstanceId_;
+        int32_t lastInstanceId_;
+    };
+
+    static GfxRuntimeMeta& getInstance(void) noexcept;
+
+    GfxRuntimeMeta(GfxRuntimeMeta const&) = delete;
+    GfxRuntimeMeta(GfxRuntimeMeta&&) = delete;
+
+    GfxRuntimeMeta& operator=(GfxRuntimeMeta const&) = delete;
+    GfxRuntimeMeta& operator=(GfxRuntimeMeta&&) = delete;
+
+    ClassInfo const& getClassInfo(const int32_t index) noexcept;
+    int32_t getClassCount(void) const noexcept;
+
+    void constructObject(const char * className, int32_t instanceId) noexcept;
+    void destructObject(const char * className, int32_t instanceId) noexcept;
+
+    std::ostream& printToStream(std::ostream& ostream) const noexcept;
+private:
+    GfxRuntimeMeta() noexcept;
+    ~GfxRuntimeMeta() noexcept;
+
+    void clear(void) noexcept;
+
+    std::unordered_map<std::string, ClassInfo> classUMap;
+
+    ClassInfo classInfo_;
+};
+
+}  // namespace _gfx
+
+}  // namespace gfx
+
+#endif  // GfxRuntimeMeta_hpp
