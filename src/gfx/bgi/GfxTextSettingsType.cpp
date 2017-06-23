59b3b791db6464aa41b399afdedc1401edbabf08
Familia Oros <familiaoros@Familias-iMac.local>
2017-06-23 19:12:32 +0300

    Add class GfxTextSettingsType

---
+++ src/gfx/bgi/GfxTextSettingsType.cpp
@@ -0,0 +1,129 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+*/
+
+#include "GfxTextSettingsType.hpp"
+
+namespace gfx
+{
+
+namespace bgi
+{
+
+const char GfxTextSettingsType::ClassName[] = "GfxTextSettingsType";
+
+GfxTextSettingsType::GfxTextSettingsType() noexcept : GfxObject(ClassName)
+{
+    clear();
+}
+
+GfxTextSettingsType::GfxTextSettingsType(const BgiType textsettings) noexcept : GfxObject(ClassName)
+{
+    textSettings_ = textsettings;
+}
+
+GfxTextSettingsType::GfxTextSettingsType(GfxTextSettingsType const& other) noexcept : GfxObject(ClassName)
+{
+    textSettings_ = other.textSettings_;
+}
+
+GfxTextSettingsType::GfxTextSettingsType(GfxTextSettingsType&& other) noexcept : GfxObject(ClassName)
+{
+    textSettings_ = other.textSettings_;
+    // Delete other's value
+    other.clear();
+}
+
+GfxTextSettingsType& GfxTextSettingsType::operator=(GfxTextSettingsType const& other) noexcept
+{
+    if (this != &other)
+    {
+        textSettings_ = other.textSettings_;
+    }
+    return *this;
+}
+
+GfxTextSettingsType& GfxTextSettingsType::operator=(GfxTextSettingsType&& other) noexcept
+{
+    if (this != &other)
+    {
+        textSettings_ = other.textSettings_;
+        // Delete other's value
+        other.clear();
+    }
+    return *this;
+}
+
+GfxTextSettingsType::operator bool() const noexcept
+{
+    return true;
+}
+
+GfxFonts GfxTextSettingsType::getFont(void) const noexcept
+{
+    return GfxFonts(textSettings_.font);
+}
+
+GfxDirection GfxTextSettingsType::getDirection(void) const noexcept
+{
+    return GfxDirection(textSettings_.direction);
+}
+
+int32_t GfxTextSettingsType::getCharSize(void) const noexcept
+{
+    return textSettings_.charsize;
+}
+
+GfxTextJustification GfxTextSettingsType::getHorizontalJustification(void) const noexcept
+{
+    return GfxTextJustification(textSettings_.horiz);
+}
+
+GfxTextJustification GfxTextSettingsType::getVerticalJustification(void) const noexcept
+{
+    return GfxTextJustification(textSettings_.vert);
+}
+
+void GfxTextSettingsType::setValue(const BgiType textsettings) noexcept
+{
+    textSettings_ = textsettings;
+}
+
+void GfxTextSettingsType::clear(void) noexcept
+{
+    textSettings_.font = prv::GfxCanvasBgi::bgiFonts::DEFAULT_FONT;
+    textSettings_.direction = prv::GfxCanvasBgi::bgiDirection::HORIZ_DIR;
+    textSettings_.charsize = -1;
+    textSettings_.horiz = prv::GfxCanvasBgi::bgiTextJustification::LEFT_TEXT;
+    textSettings_.vert = prv::GfxCanvasBgi::bgiTextJustification::TOP_TEXT;
+}
+
+GfxTextSettingsType::BgiType GfxTextSettingsType::getAsBgiType(void) const noexcept
+{
+    return textSettings_;
+}
+
+}  // namespace bgi
+
+}  // namespace gfx
+
+/* EOF */
---
+++ src/gfx/bgi/GfxTextSettingsType.hpp
@@ -0,0 +1,80 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+*/
+
+#ifndef GfxTextSettingsType_hpp
+#define GfxTextSettingsType_hpp
+
+#include <cstdint>
+
+#include "GfxObject.hpp"
+#include "GfxCanvasBgi.hpp"
+#include "GfxFonts.hpp"
+#include "GfxDirection.hpp"
+#include "GfxTextJustification.hpp"
+
+namespace gfx
+{
+
+namespace bgi
+{
+
+class GfxTextSettingsType final : public GfxObject
+{
+public:
+    typedef prv::GfxCanvasBgi::textsettingstype BgiType;
+
+    static const char ClassName[];
+    static const bool SdlResource = false;
+    static const bool CallsSdl = false;
+
+    GfxTextSettingsType() noexcept;
+
+    explicit GfxTextSettingsType(const BgiType textsettings) noexcept;
+
+    GfxTextSettingsType(GfxTextSettingsType const& other) noexcept;
+    GfxTextSettingsType(GfxTextSettingsType&& other) noexcept;
+
+    GfxTextSettingsType& operator=(const GfxTextSettingsType& other) noexcept;
+    GfxTextSettingsType& operator=(GfxTextSettingsType&& other) noexcept;
+
+    virtual explicit operator bool() const noexcept;
+
+    GfxFonts getFont(void) const noexcept;
+    GfxDirection getDirection(void) const noexcept;
+    int32_t getCharSize(void) const noexcept;
+    GfxTextJustification getHorizontalJustification(void) const noexcept;
+    GfxTextJustification getVerticalJustification(void) const noexcept;
+    void setValue(const BgiType textsettings) noexcept;
+
+    void clear(void) noexcept;
+
+    BgiType getAsBgiType(void) const noexcept;
+private:
+    BgiType textSettings_;
+};
+
+}  // namespace bgi
+
+}  // namespace gfx
+
+#endif /* GfxTextSettingsType_hpp */
