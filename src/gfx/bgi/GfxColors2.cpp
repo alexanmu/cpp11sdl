09f1d24cf9ee0f157b7e394f1d1f5ca905e1408b
Familia Oros <familiaoros@Familias-iMac.local>
2017-06-23 19:18:16 +0300

    Add class GfxColors2

---
+++ src/gfx/bgi/GfxColors2.cpp
@@ -0,0 +1,110 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+*/
+
+#include "GfxColors2.hpp"
+
+namespace gfx
+{
+
+namespace bgi
+{
+
+const char GfxColors2::ClassName[] = "GfxColors2";
+
+GfxColors2::GfxColors2() noexcept : GfxObject(ClassName)
+{
+    clear();
+}
+
+GfxColors2::GfxColors2(const ValueType clr) noexcept : GfxObject(ClassName)
+{
+    clr_ = static_cast<BgiType>(clr);
+}
+
+GfxColors2::GfxColors2(const BgiType clr) noexcept : GfxObject(ClassName)
+{
+    clr_ = clr;
+}
+
+GfxColors2::GfxColors2(GfxColors2 const& other) noexcept : GfxObject(ClassName)
+{
+    clr_ = other.clr_;
+}
+
+GfxColors2::GfxColors2(GfxColors2&& other) noexcept : GfxObject(ClassName)
+{
+    clr_ = other.clr_;
+    // Delete other's value
+    other.clear();
+}
+
+GfxColors2& GfxColors2::operator=(GfxColors2 const& other) noexcept
+{
+    if (this != &other)
+    {
+        clr_ = other.clr_;
+    }
+    return *this;
+}
+
+GfxColors2& GfxColors2::operator=(GfxColors2&& other) noexcept
+{
+    if (this != &other)
+    {
+        clr_ = other.clr_;
+        // Delete other's value
+        other.clear();
+    }
+    return *this;
+}
+
+GfxColors2::operator bool() const noexcept
+{
+    return true;
+}
+
+GfxColors2::ValueType GfxColors2::getValue(void) const noexcept
+{
+    return static_cast<ValueType>(clr_);
+}
+
+void GfxColors2::setValue(const ValueType clr) noexcept
+{
+    clr_ = static_cast<BgiType>(clr);
+}
+
+void GfxColors2::clear(void) noexcept
+{
+    clr_ = static_cast<BgiType>(ValueType::black);
+}
+
+GfxColors2::BgiType GfxColors2::getAsBgiType(void) const noexcept
+{
+    return clr_;
+}
+
+}  // namespace bgi
+
+}  // namespace gfx
+
+/* EOF */
---
+++ src/gfx/bgi/GfxColors2.hpp
@@ -0,0 +1,97 @@
+/*
+ Giotto
+ Copyright (C) 2017 George Oros
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ See copyright notice at http://lidsdl.org/license.php
+*/
+
+#ifndef GfxColors2_hpp
+#define GfxColors2_hpp
+
+#include <cstdint>
+
+#include "GfxObject.hpp"
+#include "GfxCanvasBgi.hpp"
+
+namespace gfx
+{
+
+namespace bgi
+{
+
+class GfxColors2 final : public GfxObject
+{
+public:
+    typedef prv::GfxCanvasBgi::bgiColors BgiType;
+
+    static const char ClassName[];
+    static const bool SdlResource = false;
+    static const bool CallsSdl = false;
+
+    enum class ValueType : int32_t
+    {
+        black = prv::GfxCanvasBgi::bgiColors::BLACK,
+        blue = prv::GfxCanvasBgi::bgiColors::BLUE,
+        green = prv::GfxCanvasBgi::bgiColors::GREEN,
+        cyan = prv::GfxCanvasBgi::bgiColors::CYAN,
+        red = prv::GfxCanvasBgi::bgiColors::RED,
+        magenta = prv::GfxCanvasBgi::bgiColors::MAGENTA,
+        brown = prv::GfxCanvasBgi::bgiColors::BROWN,
+        lightGray = prv::GfxCanvasBgi::bgiColors::LIGHTGRAY,
+        darkGray = prv::GfxCanvasBgi::bgiColors::DARKGRAY,
+        lightBlue = prv::GfxCanvasBgi::bgiColors::LIGHTBLUE,
+        lightGreen = prv::GfxCanvasBgi::bgiColors::LIGHTGREEN,
+        lightCyan = prv::GfxCanvasBgi::bgiColors::LIGHTCYAN,
+        lightRed = prv::GfxCanvasBgi::bgiColors::LIGHTRED,
+        lightMagenta = prv::GfxCanvasBgi::bgiColors::LIGHTMAGENTA,
+        yellow = prv::GfxCanvasBgi::bgiColors::YELLOW,
+        white = prv::GfxCanvasBgi::bgiColors::WHITE,
+        customForeground = prv::GfxCanvasBgi::bgiColors::CUSTOM_FG,
+        customBackground = prv::GfxCanvasBgi::bgiColors::CUSTOM_BG,
+        customFill = prv::GfxCanvasBgi::bgiColors::CUSTOM_FILL
+    };
+
+    GfxColors2() noexcept;
+
+    explicit GfxColors2(const ValueType clr) noexcept;
+    explicit GfxColors2(const BgiType clr) noexcept;
+
+    GfxColors2(GfxColors2 const& other) noexcept;
+    GfxColors2(GfxColors2&& other) noexcept;
+
+    GfxColors2& operator=(const GfxColors2& other) noexcept;
+    GfxColors2& operator=(GfxColors2&& other) noexcept;
+
+    virtual explicit operator bool() const noexcept;
+
+    ValueType getValue(void) const noexcept;
+    void setValue(const ValueType clr) noexcept;
+
+    void clear(void) noexcept;
+
+    BgiType getAsBgiType(void) const noexcept;
+private:
+    BgiType clr_;
+};
+
+}  // namespace bgi
+
+}  // namespace gfx
+
+#endif /* GfxColors2_hpp */
