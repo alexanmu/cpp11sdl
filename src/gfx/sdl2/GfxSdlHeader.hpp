2143d08b2ff955e130e736d0f9a7da77d4e2de7b
George Oros <george.oros@gmail.com>
2017-10-13 06:47:05 +0300

    Update for SDL2-2.0.6

--- src/gfx/sdl2/GfxSdlHeader.hpp
+++ src/gfx/sdl2/GfxSdlHeader.hpp
@@ -39,7 +39,7 @@ namespace sdl2
 }  // namespace gfx
 
 /*
-    New features, not yet present in SDL release-2.0.5; must be removed when 2.0.6 will be relased!
+    New features, not yet present in SDL release-2.0.6; must be removed when 2.0.7 (or SDL 2.1) will be relased!
     Compiler error expected here in case on conflicts.
 */
 namespace gfx
@@ -48,41 +48,7 @@ namespace gfx
 namespace sdl2
 {
 
-/* GfxJoystickType.hpp */
-/*typedef enum
-{
-    SDL_JOYSTICK_TYPE_UNKNOWN,
-    SDL_JOYSTICK_TYPE_GAMECONTROLLER,
-    SDL_JOYSTICK_TYPE_WHEEL,
-    SDL_JOYSTICK_TYPE_ARCADE_STICK,
-    SDL_JOYSTICK_TYPE_FLIGHT_STICK,
-    SDL_JOYSTICK_TYPE_DANCE_PAD,
-    SDL_JOYSTICK_TYPE_GUITAR,
-    SDL_JOYSTICK_TYPE_DRUM_KIT,
-    SDL_JOYSTICK_TYPE_ARCADE_PAR,
-    SDL_JOYSTICK_TYPE_THROTTLE
-} SDL_JoystickType;*/
-
-/* GfxHints.cpp */
-/*#define SDL_HINT_RENDER_LOGICAL_SIZE_MODE "SDL_HINT_RENDER_LOGICAL_SIZE_MODE"
-#define SDL_HINT_MOUSE_NORMAL_SPEED_SCALE "SDL_HINT_MOUSE_NORMAL_SPEED_SCALE"
-#define SDL_HINT_MOUSE_RELATIVE_SPEED_SCALE "SDL_HINT_MOUSE_RELATIVE_SPEED_SCALE"
-#define SDL_HINT_QTWAYLAND_CONTENT_ORIENTATION "SDL_HINT_QTWAYLAND_CONTENT_ORIENTATION"
-#define SDL_HINT_QTWAYLAND_WINDOW_FLAGS "SDL_HINT_QTWAYLAND_WINDOW_FLAGS"*/
-#define SDL_HINT_OPENGLES_VIDEO_DRIVER "SDL_HINT_OPENGLES_VIDEO_DRIVER"
-/*#define SDL_HINT_AUDIO_RESAMPLING_MODE "SDL_HINT_AUDIO_RESAMPLING_MODE"*/
-
-/* GfxJoystick.cpp */
-/*inline int SDL_JoystickGetDeviceVendor(int) { return 0; }
-inline int SDL_JoystickGetDeviceProduct(int) { return 0; }
-inline int SDL_JoystickGetDeviceProductVersion(int) { return 0; }
-inline SDL_JoystickType SDL_JoystickGetDeviceType(int) { return static_cast<SDL_JoystickType>(0); }
-inline SDL_JoystickID SDL_JoystickGetDeviceInstanceID(int) { return static_cast<SDL_JoystickID>(-1); }
-inline Uint16 SDL_JoystickGetVendor(SDL_Joystick *) { return 0; }
-inline Uint16 SDL_JoystickGetProduct(SDL_Joystick *) { return 0; }
-inline Uint16 SDL_JoystickGetProductVersion(SDL_Joystick *) { return 0; }
-inline SDL_JoystickType SDL_JoystickGetType(SDL_Joystick *) { return static_cast<SDL_JoystickType>(0); }
-inline SDL_bool SDL_JoystickGetAxisInitialState(SDL_Joystick *, int, Sint16 *) { return static_cast<SDL_bool>(0); }*/
+/* Empty for now */
 
 }  // namespace sdl2
 
